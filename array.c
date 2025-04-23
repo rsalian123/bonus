8b61d1c5b887ea6c605c6d7ec155dd3c6899562c
Adam Gohain <adamgohain@gmail.com>
2025-04-22 20:18:35 -0400

    more efficient sum

--- array.c
+++ array.c
@@ -22,11 +22,9 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < n; i++) {
         arr[i] = i + 1;
     }
-    
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += arr[i];
-    }
+
+    // gauss addition of sequence of consecutive natural numbers
+    int sum = n * (n + 1) / 2;
     
     printf("Sum of array = %d\n", sum);
     
