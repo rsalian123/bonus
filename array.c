1d614e6db521b8d9b882589136ecfb4e37d2607d
Adam Gohain <adamgohain@gmail.com>
2025-04-22 20:20:35 -0400

    add optional timing argument

--- array.c
+++ array.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     int n = 10;
+    int show_time = 0;
+    clock_t start, end;
+    double cpu_time_used;
     
-    if (argc > 1) {
-        n = atoi(argv[1]);
-        if (n <= 0) {
-            printf("Array size must be a positive integer\n");
-            return 1;
+    // parse command line arguments
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--time") == 0 || strcmp(argv[i], "-t") == 0) {
+            show_time = 1;
+        } else {
+            n = atoi(argv[i]);
+            if (n <= 0) {
+                printf("Array size must be a positive integer\n");
+                return 1;
+            }
         }
+    }
+    
+    if (show_time) {
+        start = clock();
     }
     
     int *arr = malloc(n * sizeof(int));
@@ -29,5 +43,12 @@ int main(int argc, char *argv[]) {
     printf("Sum of array = %d\n", sum);
     
     free(arr);
+    
+    if (show_time) {
+        end = clock();
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        printf("Time taken: %f seconds\n", cpu_time_used);
+    }
+    
     return 0;
 }
